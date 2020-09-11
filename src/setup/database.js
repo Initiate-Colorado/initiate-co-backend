1f23e871bb150bc3c9ff9bed3d37b2cde74af38f
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-09-11 16:34:49 -0600

    uncomment the connection then and catch statements to verify heroku

--- src/setup/database.js
+++ src/setup/database.js
@@ -20,11 +20,11 @@ const connection = new Sequelize(databaseConfigEnv.database, databaseConfigEnv.u
 //
 connection
   .authenticate()
-//   .then(() => {
-//     console.info('INFO - Database connected.')
-//   })
-//   .catch(err => {
-//     console.error('ERROR - Unable to connect to the database:', err)
-//   })
+  .then(() => {
+    console.info('INFO - Database connected.')
+  })
+  .catch(err => {
+    console.error('ERROR - Unable to connect to the database:', err)
+  })
 
 export default connection
