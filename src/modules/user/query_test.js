a0ee984f49a27b183402edef468b0b133812bf21
michelle <foleymichelle9@gmail.com>
2020-09-13 17:17:28 -0500

    setup

--- src/modules/user/query_test.js
+++ src/modules/user/query_test.js
@@ -2,3 +2,18 @@ import request from 'supertest'
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from '../../setup/schema'
+
+describe('user_queries', () => {
+  let server;
+
+  beforeAll(() => {
+    server = express();
+
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false,
+      })
+    )
+  })
