6fac90d74a9a1a9b843486b489761c175a8d08a1
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-09-13 12:20:16 -0600

    remove database.json, restructure configenv file to pull from .env

--- src/config/env.js
+++ src/config/env.js
@@ -1,11 +1,21 @@
 // Imports
 import dotenv from 'dotenv'
-
+import path from 'path'
 // Load .env
-dotenv.config()
+dotenv.config({path: path.resolve(`${__dirname}/../..`, '.env')})
 
 // Environment
 export const NODE_ENV = process.env.NODE_ENV
 
 // Port
 export const PORT = process.env.PORT || 8000
+
+// Username
+export const ENV = {
+  HOST: process.env.HOST,
+  DATABASE: process.env.DATABASE,
+  USERNAME: process.env.USER,
+  PASSWORD: process.env.PASSWORD
+}
+
+// wrap up node_ENV and PORT for later
