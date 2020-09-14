3c7100dccbcbccc52d2ece941aebf5cbbba0dfde
Mariana Cid <cidmariana5@gmail.com>
2020-09-14 10:42:04 -0600

    query test for ballots

---
+++ src/modules/ballots/query.test.js
@@ -0,0 +1,56 @@
+import request from 'supertest'
+import express from 'express'
+import schema from '../../setup/schema'
+import graphqlHTTP from 'express-graphql'
+
+describe('ballot query', () => {
+  let server;
+
+  beforeAll(() => {
+    server = express();
+
+    server.use(
+      "/",
+      graphqlHTTP({
+        schema: schema,
+        graphiql: true
+      })
+    )
+  })
+
+  it("is true", () => {
+    expect(true).toBe(true)
+  })
+
+  it("returns Ballot by id", async () => {
+    const response = await request(server)
+    .get('/')
+    .send({
+      query: `{ballot(id: 1){
+        id
+        type
+        subject
+        description
+      }}`
+    })
+    .expect(200)
+    console.log(response.body)
+    expect(response.body.data.ballot.length).toEqual(1)
+  })
+
+  it("returns all Ballots", async () => {
+    const response = await request(server)
+    .get('/')
+    .send({
+      query: `{ballots{
+        id
+        type
+        subject
+        description
+      }}`
+    })
+    .expect(200)
+    console.log(response.body)
+    // expect(response.body.data.ballot.length).toEqual(1)
+  })
+})
