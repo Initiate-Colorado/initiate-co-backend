23e2d4fbe6ebdcc634f623c065290623f74d7f32
michelle <foleymichelle9@gmail.com>
2020-09-15 16:47:56 -0500

    cleanup test file

--- __tests__/modules/userMutations.test.js
+++ __tests__/modules/userMutations.test.js
@@ -1,33 +1,4 @@
-// import request from 'supertest';
-// import express from 'express';
-// import graphqlHTTP from 'express-graphql';
-// import schema from '../../src/setup/schema';
-// import models from '../../src/setup/models';
-//
-// describe("User mutations", () => {
-//   let server = express();
-//   beforeAll(async () => {
-//     server.use(
-//       '/',
-//       graphqlHTTP({
-//         schema: schema,
-//         graphiql: false
-//       })
-//     );
-//
-//     it('userSignup', async () => {
-//       const response = await request(server)
-//       .get('/')
-//       .send({ mutation: 'mutation {userSignup( name: "User3", email: "user3@initiateco.com", password: "123456") {name, email}}' })
-//       .expect(200)
-//       expect(response.body.data.userSignup.name).toEqual("User3")
-//       expect(response.body.data.userSignup.name).toEqual("user3@initiateco.com")
-//     });
-//   });
-//     afterAll(() => {
-//     server.close();
-//   });
-// })
+
 import request from 'supertest';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
@@ -54,12 +25,7 @@ describe("User queries", () => {
       expect(response.body.data.userSignup.name).toEqual("craig")
       expect(response.body.data.userSignup.email).toEqual("craig@initiateco.com")
       models.User.destroy({ where: {id: response.body.data.userSignup.id} })
-      // const response2 = await request(server)
-      // .post('/')
-      // .send({ query: `mutation {userRemove( id: ${response.body.data.userSignup.id})}` })
-      // .expect(200)
     });
-  // });
   afterAll(() => {
   server.close();
 });
