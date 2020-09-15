75f422f9b56516619ecbe778daf49d32d0e37098
michelle <foleymichelle9@gmail.com>
2020-09-15 17:58:26 -0500

    update

--- __tests__/modules/userMutations.test.js
+++ __tests__/modules/userMutations.test.js
@@ -4,7 +4,7 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from '../../src/setup/schema';
 import models from '../../src/setup/models';
-describe("User queries", () => {
+describe("User mutations", () => {
   let token = ""
   let server = express();
   beforeAll(async () => {
@@ -25,9 +25,9 @@ describe("User queries", () => {
       expect(response.body.data.userSignup.name).toEqual("craig")
       expect(response.body.data.userSignup.email).toEqual("craig@initiateco.com")
       models.User.destroy({ where: {id: response.body.data.userSignup.id} })
-    });
+  });
 
-it('userRemove', async () => {
+  it('userRemove', async () => {
   models.User.create({id: 5, name: "craig", email: "craig@initiateco.com", password: "1234"})
     const response = await request(server)
     .post('/')
@@ -36,7 +36,21 @@ it('userRemove', async () => {
     expect(response.body.data.userRemove.id).toEqual(null)
     expect(response.body.data.userRemove.email).toEqual(null)
     expect(response.body.data.userRemove.name).toEqual(null)
-    });
+  });
+
+    // it('updateUser', async () => {
+    //   const craig = models.User.create({id: 6, name: "craig1", email: "craig1@initiateco.com", password: "1234"})
+    //     const response = await request(server)
+    //     .post('/')
+    //     .send({ query: `mutation { updateUser( id: ${craig.id}, email: 'craig2@initiateco.com', ballotTitle: 'Frogs suck', ballotDescription: 'self explainitory') {email, id, ballotTitle, ballotDescription}}` })
+    //     .expect(200)
+    //     console.log (response.body)
+    //     // expect(response.body.data.updateUser.id).toEqual(5)
+    //     // expect(response.body.data.updateUser.email).toEqual("craig2@initiateco.com")
+    //     // expect(response.body.data.updateUser.ballotTitle).toEqual("Frogs suck")
+    //     // expect(response.body.data.updateUser.ballotDescription).toEqual("self explainitory")
+    //     models.User.destroy({ where: {id: craig.id} })
+    // });
     afterAll(() => {
     server.close();
     });
