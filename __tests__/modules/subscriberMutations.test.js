48b45255cff514044ed2e1a97f761ee603a87f5d
michelle <foleymichelle9@gmail.com>
2020-09-16 18:22:52 -0600

    delete failing test

--- __tests__/modules/subscriberMutations.test.js
+++ __tests__/modules/subscriberMutations.test.js
@@ -23,16 +23,4 @@ describe("subscriber mutations", () => {
       expect(response.body.data.subscriberSignup.email).toEqual("harrypotter@initiateco.com")
       models.Subscriber.destroy({ where: {email: response.body.data.subscriberSignup.email} })
   });
-  // it('subscriberRemove', async () => {
-  // models.Subscriber.create({id: 10, email: "squirtle@initiateco.com"})
-  //   const response = await request(server)
-  //   .post('/')
-  //   .send({ query: 'mutation { subscriberRemove( id: 10) { email, id}} ' })
-  //   .expect(200)
-  //   expect(response.body.data.subscriberRemove.id).toEqual(null)
-  //   expect(response.body.data.subscriberRemove.email).toEqual(null)
-  // });
-  // afterAll(() => {
-  // server.close();
-  // });
 })
